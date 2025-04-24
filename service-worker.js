054311ed9d0d02f7ce8c7ed7f8fecdf0097f3da3
remarkablemark <remarkablemark@users.noreply.github.com>
2025-04-24 01:28:26 +0000

    Deploying to gh-pages from @ remarkablegames/room-crawler@7a946385f3557013fc447d1c7bd93d57722c266d 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'ren-py-roguelike';
+var cacheName = 'room-crawler';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
